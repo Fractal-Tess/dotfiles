ab2b6c6e313200a98d350c5d1410ae85dba0978a
Fractal-Tess <vgfractal@gmail.com>
2024-02-12 19:32:00 +0200

    track custom

---
+++ nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,67 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "svelte",
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "prettier",
+    "tailwindcss-language-server",
+    "eslint-lsp",
+    "svelte-language-server",
+
+    -- rust
+    "rust-analyzer",
+    "rustfmt",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
