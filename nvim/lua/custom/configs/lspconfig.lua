9ed231d8298f9b21d6f4f6067ca24739cf0c611b
Fractal-Tess <vgfractal@gmail.com>
2024-04-06 23:34:05 +0300

    s

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -4,9 +4,19 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver",
-  "clangd", "svelte", "tailwindcss", "rust_analyzer",
-  "eslint", "emmet_language_server", "nil_ls"}
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "svelte",
+  "tailwindcss",
+  "rust_analyzer",
+  "eslint",
+  "emmet_language_server",
+  "nil_ls",
+  "csharp_ls", -- dotnet sdk
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -15,5 +25,5 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
+--
 -- lspconfig.pyright.setup { blabla }
